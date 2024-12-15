7b6383437cf85f698aca09e2c008d3732163d5e7
Alireza98-1 <alirezaghaem97@gmail.com>
2024-12-15 13:56:27 +0330

    Feature: Add admin panel customization for forms and questions

--- forms/admin.py
+++ forms/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Form, ShortTextQuestion, LongTextQuestion, EmailQuestion, NumericQuestion
 
-# Register your models here.
+
+class QuestionInline(admin.TabularInline):
+    model = None  # به صورت داینامیک مشخص می‌شود
+    extra = 1
+
+
+@admin.register(Form)
+class FormAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    inlines = []
+
+    def get_inlines(self, request, obj=None):
+        self.inlines = []
+        for model in [ShortTextQuestion, LongTextQuestion, EmailQuestion, NumericQuestion]:
+            inline = type(f"{model.__name__}Inline", (QuestionInline,), {"model": model})
+            self.inlines.append(inline)
+        return super().get_inlines(request, obj)
+
+
+@admin.register(ShortTextQuestion)
+class ShortTextQuestionAdmin(admin.ModelAdmin):
+    list_display = ('text', 'max_length', 'form')
+
+
+@admin.register(LongTextQuestion)
+class LongTextQuestionAdmin(admin.ModelAdmin):
+    list_display = ('text', 'max_length', 'form')
+
+
+@admin.register(EmailQuestion)
+class EmailQuestionAdmin(admin.ModelAdmin):
+    list_display = ('text', 'form')
+
+
+@admin.register(NumericQuestion)
+class NumericQuestionAdmin(admin.ModelAdmin):
+    list_display = ('text', 'min_value', 'max_value', 'is_decimal_allowed', 'form')
